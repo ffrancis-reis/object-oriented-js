40ee1849dbb1fab6ac7f952f6bbf348504b0f46f
Felipe Reis <felipe.reis_ext@interplayers.com.br>
2020-10-19 17:51:51 -0300

    Still messing up with function and objects

--- app.js
+++ app.js
@@ -1,17 +1,78 @@
-fetch("./dino.json", { mode: "no-cors" })
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.error(error));
-
-const dino = {
-  species: "Triceratops",
-  weight: 13000,
-  height: 114,
-  diet: "herbavor",
-  where: "North America",
-  when: "Late Cretaceous",
-  fact: "First discovered in 1889 by Othniel Charles Marsh",
-};
+const Dinos = [
+  {
+    species: "Triceratops",
+    weight: 13000,
+    height: 114,
+    diet: "herbavor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "First discovered in 1889 by Othniel Charles Marsh",
+  },
+  {
+    species: "Tyrannosaurus Rex",
+    weight: 11905,
+    height: 144,
+    diet: "carnivor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "The largest known skull measures in at 5 feet long.",
+  },
+  {
+    species: "Anklyosaurus",
+    weight: 10500,
+    height: 55,
+    diet: "herbavor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "Anklyosaurus survived for approximately 135 million years.",
+  },
+  {
+    species: "Brachiosaurus",
+    weight: 70000,
+    height: "372",
+    diet: "herbavor",
+    where: "North America",
+    when: "Late Jurasic",
+    fact: "An asteroid was named 9954 Brachiosaurus in 1991.",
+  },
+  {
+    species: "Stegosaurus",
+    weight: 11600,
+    height: 79,
+    diet: "herbavor",
+    where: "North America, Europe, Asia",
+    when: "Late Jurasic to Early Cretaceous",
+    fact:
+      "The Stegosaurus had between 17 and 22 seperate places and flat spines.",
+  },
+  {
+    species: "Elasmosaurus",
+    weight: 16000,
+    height: 59,
+    diet: "carnivor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "Elasmosaurus was a marine reptile first discovered in Kansas.",
+  },
+  {
+    species: "Pteranodon",
+    weight: 44,
+    height: 20,
+    diet: "carnivor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "Actually a flying reptile, the Pteranodon is not a dinosaur.",
+  },
+  {
+    species: "Pigeon",
+    weight: 0.5,
+    height: 9,
+    diet: "herbavor",
+    where: "World Wide",
+    when: "Holocene",
+    fact: "All birds are living dinosaurs.",
+  },
+];
 
 // Create Dino Constructor
 function Dinosaur(dinosaur) {
@@ -25,35 +86,45 @@ function Dinosaur(dinosaur) {
 }
 
 // Create Dino Objects
-const dinosaur = new Dinosaur(dino);
+const dinosaurs = [];
+Dinos.forEach(function (dino) {
+  dinosaurs.push(new Dinosaur(dino));
+});
 
 // Create Human Object
 const human = {
-  species: "Triceratops",
-  weight: 13000,
-  height: 114,
-  diet: "herbavor",
+  species: "human",
   where: "North America",
   when: "Late Cretaceous",
   fact: "First discovered in 1889 by Othniel Charles Marsh",
 };
 
 // Use IIFE to get human data from form
-(function getHumanData() {
-  const nameElement = document.getElementById("name");
-  const feetElement = document.getElementById("feet");
-  const inchesElement = document.getElementById("inches");
-  const weightElement = document.getElementById("weight");
-  const dietElement = document.getElementById("diet");
+const button = document.getElementById("btn");
 
-  return function () {
-    name: nameElement.value;
-    feet: feetElement.value;
-    inches: inchesElement.value;
-    weight: weightElement.value;
-    diet: dietElement.value;
-  };
-})();
+button.addEventListener("click", () => {
+  (function (human) {
+    const nameElement = document.getElementById("name");
+    const feetElement = document.getElementById("feet");
+    const inchesElement = document.getElementById("inches");
+    const weightElement = document.getElementById("weight");
+    const dietElement = document.getElementById("diet");
+
+    return function () {
+      human.name = nameElement.value;
+      human.feet = feetElement.value;
+      human.inches = inchesElement.value;
+      human.weight = weightElement.value;
+      human.diet = dietElement.value;
+    };
+  })();
+});
+
+// human.name = getHumanData().name;
+// human.feet = getHumanData().feet;
+// human.inches = getHumanData().inches;
+// human.weight = getHumanData().weight;
+// human.diet = getHumanData().diet;
 
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches.
@@ -68,8 +139,7 @@ function compareMeasurements(human, dinosaur) {
       : "You are smaller than this dinosaur!";
 
   return {
-    weightMeasure: weight,
-    heightMeasure: height,
+    measuresComparison: `${weight} ${height}`,
   };
 }
 
@@ -94,27 +164,37 @@ function compareSpecies(dinosaur) {
 }
 
 // Generate Tiles for each Dino in Array
-function generateTiles(dinos) {
-  let dinosaurs = [];
+function generateTiles() {
+  const tiles = [];
 
-  dinos.forEach((dino) => {
+  let generateTile = function (data) {
     const item = document.createElement("div");
     item.classList.add("grid-item");
 
     const header = document.createElement("h3");
-    header.textContent = dino.species;
+    header.textContent = data.name || data.species;
 
     const image = document.createElement("img");
-    const imageFile = dino.species.toLowerCase();
+    const imageFile = data.species.toLowerCase();
     image.src = `images/${imageFile}.png`;
 
     const text = document.createElement("p");
-    text.textContent = dino.fact;
+    text.textContent = data.fact;
 
-    dinosaurs.push({ item, header, image, text });
+    return { item, header, image, text };
+  };
+
+  dinosaurs.forEach((dino) => {
+    let dinoTile = generateTile(dino);
+
+    tiles.push(dinoTile);
   });
 
-  return dinosaurs;
+  const humanTile = generateTile(human);
+
+  tiles.push(humanTile);
+
+  return tiles;
 }
 
 // Add tiles to DOM
@@ -137,21 +217,13 @@ function removeForm() {
 }
 
 // On button click, prepare and display infographic
-const button = document.getElementById("btn");
-
 button.addEventListener("click", () => {
-  // const human = getHumanData();
+  console.log(compareMeasurements(human, dinosaurs[0]));
+  console.log(compareDiet(human, dinosaurs[0]));
+  console.log(compareSpecies(dinosaurs[0]));
 
-  console.log(compareMeasurements(human, dino));
-  console.log(compareDiet(human, dino));
-  console.log(compareSpecies(dino));
-
-  const dinosaurs = [];
-  dinosaurs.push(dino);
-
-  const dinosaursTiles = generateTiles(dinosaurs);
-
-  addTilesToGrid(dinosaursTiles);
+  const tiles = generateTiles();
+  addTilesToGrid(tiles);
 
   removeForm();
 });
